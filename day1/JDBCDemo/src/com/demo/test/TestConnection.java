0ecfd4a7a0f7d4096631365e8aa834fc8cd1fbaf
Ashish Mahajan <aashishmahajan01@gmail.com>
2023-06-16 21:58:57 +0530

    day1

---
+++ day1/JDBCDemo/src/com/demo/test/TestConnection.java
@@ -0,0 +1,43 @@
+package com.demo.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		//step 1 -register the driver
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			//step2 : getConnection
+			String url = "jdbc:mysql://localhost:3306/test";
+			Connection con=DriverManager.getConnection(url,"root","root123");
+			if(con!=null) {
+				System.out.println("connection done");
+			}
+			else {
+				System.out.println("connection not done");
+			}
+			//step 3: create connection
+			Statement st=con.createStatement();
+			
+			//step 4 execute query
+			ResultSet rs=st.executeQuery("select * from employee");
+			while(rs.next()) {
+				System.out.println("Id : "+rs.getInt(1));
+				System.out.println("Name : "+rs.getString(2));
+				System.out.println("Salary : "+rs.getDouble(3));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
